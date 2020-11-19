c5c01958597dfe4f0bf90ef1a779ab36b7f6a5af
Thyago <t.y_josef7@hotmail.com>
2020-11-18 23:22:26 -0300

    algoritimos do livro 500 algoritimos reoslvidos mas na forma js e com alguns outros algoritimos

---
+++ alg2.js
@@ -0,0 +1,22 @@
+var chiclete = true;
+var dordedente = true;
+var lixo = true;
+
+console.log("Um acho que vou pegar um chiclete");
+if(chiclete == true){
+    console.log("peguei o chiclete")
+    }if(chiclete == true){
+        console.log("retirar o papel do chiclete")
+        }if(dordedente && chiclete == false){
+            console.log("mastigar")
+            }if(lixo == true){
+                console.log("Jogar papel fora no lixo")
+                    }else{
+                        console.log("erro 404")
+                    }
+                              
+       
+   
+            
+            
+           
---
+++ log1.js
@@ -0,0 +1,128 @@
+/*var frigideira = 1;
+var ovo = 1;
+var oleo = 1;
+var sal = 1;
+var fogo = 1;
+var x = 5;
+var preparandoo = 0;
+var y = 1;
+
+setTimeout(function pronto(){
+    console.log("pronto vou pegar")
+},2000);
+setTimeout(function preparo(){
+    console.log("pronto")
+    
+    },6000);
+
+
+
+if(frigideira == true ) {
+    console.log("Peguei a frigideira")
+    if(ovo && oleo && sal == true){
+        console.log("OS materiais necessários estão a mesa")
+        if(fogo == true ){
+            console.log("vou acender o fogo");
+            if(frigideira && fogo == true){
+                console.log("vou por a frigideira no fogo");
+                while(x!=0){
+                console.log("Oleo esquentando em " + x + "minutos")
+                x--;
+
+                }if(ovo && frigideira && fogo == true){
+                    console.log("por o ovo na frigideira")
+                    //return preparandoo = true;
+                }if(preparandoo == true){
+                    console.log("aguarde o preparo");
+                    preparo();
+            }
+           
+                
+
+
+            }
+    }else{
+        console.log("o gaz acabou");
+    }
+    
+    }else if(ovo == false) {
+        console.log("Poxa faltou o ovo não vou poder continuar");
+    }else if(oleo == false) {
+        console.log("Poxa faltou o oleo não vou pdoer continuar");
+    }else if(sal == false) {
+        console.log("Poxa falou o sal não vou poder continuar");
+    }
+    else{
+    console.log("Sem frigideira não da para fazer nada")
+    }
+}*/
+
+
+/*Algoritimo um usando crosure --- A função é chamada dentro de uma função tendos seus paramentros das 
+variaveis criadas dentro da função a que foi atribuida*/
+
+/*function frigideira(){
+     var oleo = true;
+     var fogo = true;               
+            function alimentos(){
+                if(oleo && fogo == true){
+                    console.log("esta pronto")
+                }else{
+                    console.log("não esta pronto")
+                }
+                    
+        }
+                alimentos();
+            }
+             frigideira();
+
+
+             function frigideira(){
+     var oleo = true;
+     var fogo = true;               
+            function alimentos(){
+                if(oleo && fogo == true){
+                    console.log("esta pronto")
+                }else{
+                    console.log("não esta pronto")
+                }
+                    
+        }
+                alimentos();
+            }
+             frigideira();*/
+
+
+             function frigideira(){
+                var oleo = true;
+                var fogo = true;               
+                       function alimentos(){
+                           if(oleo && fogo == true){
+                               console.log("esta pronto")
+                           }else{
+                               console.log("não esta pronto")
+                           }
+                               
+                   }
+                           return alimentos;
+                       }
+                        var preparo = frigideira();
+                        preparo();
+            
+
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
